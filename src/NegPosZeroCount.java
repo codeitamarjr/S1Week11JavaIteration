c4b07e71b5ca930b30982128188cdb18e85d05d4
Itamar Junior <itjunior@gmail.com>
2021-04-13 15:42:18 +0100

    Correct add total positive numbers

--- src/NegPosZeroCount.java
+++ src/NegPosZeroCount.java
@@ -6,31 +6,30 @@ public class NegPosZeroCount {
         System.out.println("Enter a number:");
         Scanner keyboard = new Scanner(System.in); //created a new object keyboard.
         int numberTested = keyboard.nextInt();
-        int counterWhilePositive =0 ,counterWhileNegative = 0, counterWhileZero = 0;
+        int counterWhilePositive =0 ,counterWhileNegative = 0, counterWhileZero = 0, totalPositive=0, totalNegative=0;
 
-        while (numberTested != -999){
+        while (numberTested != -999){ //While the numbers are different of -999(the sentinel value) Java will read bellow
             if(numberTested>0){
-                ++counterWhilePositive;
-                System.out.println("You entered "+counterWhilePositive+" positive numbers");
-            }
-
-            if(numberTested<0){
-                ++counterWhileNegative;
-                System.out.println("You entered "+counterWhileNegative+" negative numbers");
+                counterWhilePositive++; //start count positive numbers
+                totalPositive+=numberTested; //start to add positive numbers together
                 }
-
+            if(numberTested<0){
+                counterWhileNegative++; //start count negative numbers
+                totalNegative+=numberTested; //start to add negative numbers togheter
+                }
             if(numberTested==0){
-                ++counterWhileZero;
-                System.out.println("You entered "+counterWhileZero+" numbers zero");
-            }
-
+                counterWhileZero++; //start count zero numbers
+                }
             System.out.println("Try again: ");
-            numberTested = keyboard.nextInt();
+            numberTested = keyboard.nextInt();//
+                    }
 
-        }
-
-        System.out.println("You got it, the number is -999\n" +
-                "You tried "+(counterWhileNegative+counterWhileZero+counterWhilePositive)+" times");
+        System.out.println("You got it, the number is -999");
+        System.out.println("You entered "+counterWhilePositive+" positive numbers");
+        System.out.println("You entered "+counterWhileNegative+" negative numbers");
+        System.out.println("You entered "+counterWhileZero+" numbers zero");
+        System.out.println("The total of the positive numbers are "+totalPositive);
+        System.out.println("The total of the negative numbers are "+totalNegative);
         keyboard.close(); //closed the keyboard object.
     }
 }
